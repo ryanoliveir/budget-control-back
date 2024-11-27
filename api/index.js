1bc5f1986079e45506e7c6daacd3a9c0108ac038
mathensousaa <mhenrique.sousa43@gmail.com>
2024-11-27 00:02:59 -0300

    feat: configurate project to deploy to vercel

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "../routes/userRoutes.js";
+import expenseRoutes from "../routes/expenseRoutes.js";
+import categoryRoutes from "../routes/categoryRoutes.js";
+import authRoutes from "../routes/authRoutes.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: CLIENT_URL,
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Budget Control API");
+});
+app.use("/api", userRoutes);
+app.use("/api", expenseRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", authRoutes);  
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  console.log("Link: http://localhost:3000/");
+});
+
+export default app;
--- config/database.js
+++ config/database.js
@@ -1,8 +1,21 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+import pg from 'pg';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './db.sqlite', 
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+  schema: 'budget-control'
 });
 
 export default sequelize;
